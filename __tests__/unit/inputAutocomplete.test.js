5d5b84915cb88c7cf12800e22def12536821a255
Ramon <ramonvictorn@gmail.com>
2019-10-09 19:52:46 -0300

    version 1 done

--- __tests__/unit/inputAutocomplete.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import InputAutoComplete from '../../src/web/private/js/presentational/InputAutoComplete.js'
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<InputAutoComplete></InputAutoComplete>)
-  expect(tree).toMatchSnapshot();
-});
